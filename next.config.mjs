c3cfd3328e4a7bb0e0a8a474fdab6881dc5f812b
YSL-Home <150267819+YSL-Home@users.noreply.github.com>
2026-04-14 16:13:05 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  poweredByHeader: false,
-  generateEtags: false
+  output: "export",
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
 };
 
 export default nextConfig;
