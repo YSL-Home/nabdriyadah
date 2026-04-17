32d213166dc5803a43747c3fe22505a47c91d0ed
YSL-Home <150267819+YSL-Home@users.noreply.github.com>
2026-04-17 16:20:51 +0100

    Update sitemap.js

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,8 +3,8 @@ import articles from "../content/articles/seo-articles.json";
 export default function sitemap() {
   const baseUrl = "https://nabdriyadah.com";
 
-  const articleUrls = articles.map((_, index) => ({
-    url: `${baseUrl}/article/?id=${index + 1}`,
+  const articleUrls = articles.map((article) => ({
+    url: `${baseUrl}/articles/${article.slug}/`,
     lastModified: new Date()
   }));
 
